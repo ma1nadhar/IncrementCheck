3ce89e1b74d37e482b9bcc1d90f77c2a232497be
Maina Dhar <dmaina2244@gmail.com>
2020-02-05 09:56:39 -0800

    Add files via upload

---
+++ IncrementCheck.java
@@ -0,0 +1,12 @@
+
+public class IncrementCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k = 0;
+			for(int i=0;i<10;i++) {
+				System.out.println(k++);
+			}
+	}
+
+}
